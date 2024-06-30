dd3db6d30ed94f0ac0d9ea93cbfa26ec9f5246f6
Muhammad-ali234 <muhammadali.chd12@gmail.com>
2024-06-30 12:37:46 +0500

    pump register screen added

--- lib/Dashboared/sidebar.dart
+++ lib/Dashboared/sidebar.dart
@@ -1,132 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:myproject/Dashboared/Chat%20room/dashbored_chat.dart';
-// import 'package:myproject/Dashboared/Employee/employess_screen.dart';
-// import 'package:myproject/Dashboared/Dashboared/Screens/Home_screen.dart';
-// import 'package:myproject/Authentication/login_screen.dart';
-// import 'package:myproject/Dashboared/Pump%20Card%20Dahsboared/account_request_screen.dart';
-// import 'package:myproject/Dashboared/Pump%20Card%20Dahsboared/petrol_price.dart';
 
-// class CustomDrawer extends StatelessWidget {
-//   const CustomDrawer({
-//     super.key,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Drawer(
-//       child: Container(
-//         decoration: BoxDecoration(
-//           color: Colors.white,
-//           boxShadow: [
-//             BoxShadow(
-//               color: Colors.grey.withOpacity(0.3),
-//               spreadRadius: 5,
-//               blurRadius: 7,
-//               offset: const Offset(3, 0), // Offset to the right
-//             ),
-//           ],
-//         ),
-//         child: ListView(
-//           padding: EdgeInsets.zero,
-//           children: [
-//             const SizedBox(height: 5),
-//             ListTile(
-//               leading: const Icon(
-//                 Icons.dashboard,
-//                 color: Colors.teal,
-//               ),
-//               title: const Text('Dashboard'),
-//               onTap: () {
-//                 Navigator.pop(context); // Close the drawer
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                       builder: (context) => const DashboardOwnerScreen()),
-//                 );
-//               },
-//             ),
-//             ListTile(
-//               leading: const Icon(
-//                 Icons.perm_data_setting,
-//                 color: Colors.teal,
-//               ),
-//               title: const Text('Employee Management'),
-//               onTap: () {
-//                 Navigator.pop(context);
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                       builder: (context) => const EmployeeScreen()),
-//                 );
-//               },
-//             ),
-//             ListTile(
-//               leading: const Icon(
-//                 Icons.price_change,
-//                 color: Colors.teal,
-//               ),
-//               title: const Text('Petrol Price'),
-//               onTap: () {
-//                 Navigator.pop(context);
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                       builder: (context) => const PetrolPriceScreen()),
-//                 );
-//               },
-//             ),
-//             const SizedBox(height: 5),
-//             ListTile(
-//               leading: const Icon(
-//                 Icons.account_box,
-//                 color: Colors.teal,
-//               ),
-//               title: const Text('Requested Accounts'),
-//               onTap: () {
-//                 Navigator.pop(context);
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                       builder: (context) => const AccountRequested()),
-//                 );
-//               },
-//             ),
-//             const SizedBox(height: 5),
-//             ListTile(
-//               leading: const Icon(
-//                 Icons.account_box,
-//                 color: Colors.teal,
-//               ),
-//               title: const Text('Chat  with Us'),
-//               onTap: () {
-//                 Navigator.pop(context);
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(builder: (context) => const DashbordChat()),
-//                 );
-//               },
-//             ),
-//             const SizedBox(height: 5),
-//             ListTile(
-//               leading: const Icon(
-//                 Icons.logout,
-//                 color: Colors.teal,
-//               ),
-//               title: const Text('Logout'),
-//               onTap: () {
-//                 Navigator.pop(context);
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(builder: (context) => const LoginScreen()),
-//                 );
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:myproject/Dashboared/Chat%20room/dashbored_chat.dart';
@@ -135,6 +7,7 @@ import 'package:myproject/Dashboared/Dashboared/Screens/Home_screen.dart';
 import 'package:myproject/Authentication/login_screen.dart';
 import 'package:myproject/Dashboared/Pump%20Card%20Dahsboared/account_request_screen.dart';
 import 'package:myproject/Dashboared/Pump%20Card%20Dahsboared/petrol_price.dart';
+import 'package:myproject/Dashboared/Pump%20Reg/pump_reg.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
@@ -242,6 +115,23 @@ class CustomDrawer extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const DashbordChat()),
+                );
+              },
+            ),
+              ListTile(
+              leading: const Icon(
+                Icons.chat,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'Register a Pump',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PumpRegScreen()),
                 );
               },
             ),
